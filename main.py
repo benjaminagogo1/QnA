5e280f1431b31c3517a8e1bce03fdb55b3f797a0
benjaminagogo1 <josephbenjamin046@gmail.com>
2026-09-22 19:41:54 +0100

    number of occurrence

--- main.py
+++ main.py
@@ -1,7 +1,51 @@
+vowels = ['a', 'e', 'i', 'o', 'u']
 
-import decimal
+string = "programming"
 
-number = 10
+count = 0
 
-print(decimal.Decimal(number))
-print(type(decimal.Decimal(number)))
+for char in vowels:
+      if char in string:
+            count += 1
+
+print(count)
+
+string = "programming"
+count = 0
+
+# for char in string:
+#       if char == "m":
+#             count += 1
+# print(f"m:{count}")
+
+print()
+
+for index, value, in enumerate(string, start=7):
+      if value == "m":
+            count += 1
+            print(count)
+          
+
+
+
+
+
+from collections import Counter
+
+string = "programming"
+
+numbers = Counter(string)
+
+for char, total in numbers.items():
+      print(f" char: {char} total: {total}")
+
+
+
+print()
+
+
+counts = {}
+
+for index, char in enumerate(string):
+      counts[char] = counts.get(char, 0) + 1
+      print(f"Index: {char} Number_of_occurrence: {counts[char]}")
